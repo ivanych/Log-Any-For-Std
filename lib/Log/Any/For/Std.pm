9340139be031393d5d838b6b82cdf07d5de226a2
Mikhail Ivanov <m.ivanych@gmail.com>
2015-05-29 10:55:46 +0300

    Created skeleton module

---
+++ lib/Log/Any/For/Std.pm
@@ -0,0 +1,111 @@
+package Log::Any::For::Std;
+
+use 5.008001;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Log::Any::For::Std - The great new Log::Any::For::Std!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Log::Any::For::Std;
+
+    my $foo = Log::Any::For::Std->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Mikhail Ivanov, C<< <m.ivanych at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-log-any-for-std at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Log-Any-For-Std>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Log::Any::For::Std
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Log-Any-For-Std>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Log-Any-For-Std>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Log-Any-For-Std>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Log-Any-For-Std/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 Mikhail Ivanov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of Log::Any::For::Std
